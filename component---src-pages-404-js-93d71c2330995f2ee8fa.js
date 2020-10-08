e69c8e9b086bcc1719faceb3afc19191740ec474
christianvari <christianvari@users.noreply.github.com>
2020-10-08 20:43:37 +0000

    deploy: 2060bd15bcc010f023c0d7373ae677a7ef280215

---
+++ component---src-pages-404-js-93d71c2330995f2ee8fa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(n,t,e){"use strict";e.r(t);var c=e("Wbzz"),u=e("q1tI");t.default=function(){return Object(u.useEffect)((function(){Object(c.navigate)("/")}),[]),null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-93d71c2330995f2ee8fa.js.map
