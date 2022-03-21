22bb16a2682e4d0f9a5e8e076803fe4bb3647bb0
Christian Vari <vari.christian@gmail.com>
2022-03-21 11:11:15 +0100

    added oak security

--- src/sections/Education.js
+++ src/sections/Education.js
@@ -60,7 +60,9 @@ const Education = () => {
                   />
                   <h6 className="mt-3 font-semibold">{node.title}</h6>
                   <h6 className="text-sm">{node.subtitle}</h6>
-                  <h6 className="mt-2 text-xs">({node.period})</h6>
+                  {node.period && (
+                    <h6 className="mt-2 text-xs">({node.period})</h6>
+                  )}
                 </div>
               </div>
             );
