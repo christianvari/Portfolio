8ccd884e580c614d8165f09d310d5b7d5c8dd178
Christian Vari <vari.christian@gmail.com>
2022-11-26 16:19:02 +0100

    scroll down redesign

--- src/components/ScrollDown.js
+++ src/components/ScrollDown.js
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
+import { BsArrowDownCircle } from "react-icons/bs";
 import ThemeContext from "../context/ThemeContext";
 import { icon, container, darkTheme } from "./ScrollDown.module.css";
+import { scroller } from "react-scroll";
 
 const ScrollDown = () => {
   const { dark } = useContext(ThemeContext);
+
   return (
     <div
       className={[container, "animated", "fadeIn", dark ? darkTheme : ""].join(
@@ -11,8 +14,17 @@ const ScrollDown = () => {
       )}
       style={{ animationDelay: "2s" }}
     >
-      <div className={icon} />
-      <code>Scroll down</code>
+      <BsArrowDownCircle
+        className={icon}
+        onClick={() =>
+          scroller.scrollTo("about-me", {
+            delay: 50,
+            offset: -50,
+            duration: 600,
+            smooth: "easeInOutCubic",
+          })
+        }
+      />
     </div>
   );
 };
